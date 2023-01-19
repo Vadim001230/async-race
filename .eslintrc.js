9b3b6e002189502e7a0f915651785c5bc8878eba
Vadim Shershen <vadim.shershen.97@mail.ru>
2023-01-19 15:36:58 +0300

    feat: add webpack, eslint, tsconfig  settings

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', 'import', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ['webpack.config.js', 'data.js'],
+  rules: {
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-explicit-any': 2,
+  },
+};
